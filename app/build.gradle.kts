2a96b16e54232f47d782883acef438d1f664f90c
Brian Tafoya <btafoya@briantafoya.com>
2026-09-05 20:56:30 -0400

    Bump version to 1.1 (versionCode 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.tafoyaventures.urlsharerelay"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     signingConfigs {
